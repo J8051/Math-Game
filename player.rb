a35e2ad72f536f7af8705e07b49edeb95f06bd16
J8051 <nathaniel.jessica@yahoo.ca>
2023-05-02 16:02:56 -0400

    varibale bug fixed

--- player.rb
+++ player.rb
@@ -20,4 +20,3 @@ class Player
   end     
 end
 
-
--- question.rb
+++ question.rb
@@ -2,10 +2,10 @@ class Question
   attr_accessor :number_one, :number_two, :operator, :answer
   
   def initialize()
-    number_one = rand(1..20)
-    number_two = rand(1..20)
+    @number_one = rand(1..20)
+    @number_two = rand(1..20)
     operators = ["plus","minus","times","divided by"]
-    operator = operators[rand(0..3)]
+    @operator = operators[rand(0..3)]
     calculateAnswer(); 
   end
 
